0500332fa755399b572ae76a9ad4f9fe0cfcfd79
psides83 <psides83@hotmail.comm>
2021-11-12 14:46:10 -0600

    file management

--- src/Services/email-service.js
+++ src/Services/email-service.js
@@ -1,7 +1,7 @@
 import emailjs from 'emailjs-com';
 import moment from 'moment';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import { db } from '../services/firebase';
+import { db } from './firebase';
 
 const serviceID = 'service_5guvozs';
 const templateID = 'template_5dg1ys6';
