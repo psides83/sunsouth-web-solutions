71f9341cc2922ae0ee7ea0b576ddc9cfedafc703
Payton Sides <psides83@hotmail.com>
2022-02-28 15:12:53 -0600

    Adding transport manager

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import './styles/App.css';
-import Header from './views/Header';
-import Home from './views/Home';
+import React, { useCallback, useEffect, useState } from "react";
+import "./styles/App.css";
+import Header from "./views/Header";
+import Home from "./views/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import { useHistory } from 'react-router-dom';
 // import Login from './Login';
 import { onAuthStateChanged } from "firebase/auth";
-import { auth, db } from './services/firebase';
-import { useStateValue } from './state-management/StateProvider';
-import SignIn from './views/SignIn';
-import SignUp from './views/SignUp';
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Spinner from './components/Spinner';
-import AddRequestView from './views/AddRequestView';
-import Completed from './views/Completed';
-import AddLoanerView from './views/AddLoanerView';
-import LoanerManager from './views/table-views/LoanerManager';
-import SalesmenList from './views/table-views/SalesmenList';
-import { RequestPDF } from './components/RequestPDF';
-import { PDFViewer } from '@react-pdf/renderer';
-import { doc, getDoc } from '@firebase/firestore';
+import { auth, db } from "./services/firebase";
+import { useStateValue } from "./state-management/StateProvider";
+import SignIn from "./views/SignIn";
+import SignUp from "./views/SignUp";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Spinner from "./components/Spinner";
+import AddRequestView from "./views/AddRequestView";
+import Completed from "./views/Completed";
+import AddLoanerView from "./views/AddLoanerView";
+import LoanerManager from "./views/table-views/LoanerManager";
+import SalesmenList from "./views/table-views/SalesmenList";
+import TransportManager from "./views/table-views/TransportManager";
+import { RequestPDF } from "./components/RequestPDF";
+import { PDFViewer } from "@react-pdf/renderer";
+import { doc, getDoc } from "@firebase/firestore";
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: '#5e9655',
-      main: '#367C2B',
-      dark: '#25561e',
+      light: "#5e9655",
+      main: "#367C2B",
+      dark: "#25561e",
     },
     secondary: {
-      light: '#ffe433',
-      main: '#FFDE00',
-      dark: '#b29b00',
-    }
+      light: "#ffe433",
+      main: "#FFDE00",
+      dark: "#b29b00",
+    },
   },
 });
 
@@ -42,109 +43,124 @@ function App() {
   // const [{ pdfData }] = useStateValue();
   const [{ user }, dispatch] = useStateValue();
   const [loading, setLoading] = useState(true);
-  var [data, setData] = useState()
+  var [data, setData] = useState();
 
-  const fetchdata = async (user)=> {
-  
-    const docSnapshot = await getDoc(doc(db, 'users', user?.uid, 'pdf', 'pdfData'))
+  const fetchdata = async (user) => {
+    const docSnapshot = await getDoc(
+      doc(db, "users", user?.uid, "pdf", "pdfData")
+    );
 
     if (docSnapshot.exists()) {
       console.log("Document data:", docSnapshot.data());
-      setData(docSnapshot.data())
-      setLoading(false)
+      setData(docSnapshot.data());
+      setLoading(false);
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
     }
-  }
+  };
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      if (user) {    
+      if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
         // setTimeout( function() { setLoading(false); }, 2000);
 
         dispatch({
-          type: 'SET_USER',
+          type: "SET_USER",
           user: user,
-        })
-        fetchdata(user)
-        setTimeout( function() { setLoading(false); }, 1000);
+        });
+        fetchdata(user);
+        setTimeout(function () {
+          setLoading(false);
+        }, 1000);
       } else {
         // User is signed out
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
-        setTimeout( function() { setLoading(false); }, 500);
-      }  
+          type: "SET_USER",
+          user: null,
+        });
+        setTimeout(function () {
+          setLoading(false);
+        }, 500);
+      }
     });
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <div className="app">
         <Router>
-            <Switch>
+          <Switch>
+            <Route path="/pdf">
+              {loading ? (
+                <Spinner frame />
+              ) : (
+                <PDFViewer width="100%" height="1080">
+                  <RequestPDF
+                    className="pdf"
+                    request={data?.request}
+                    equipment={data?.equipment}
+                  />
+                </PDFViewer>
+              )}
+            </Route>
 
-              <Route path="/pdf">
-                {loading
-                ? 
-                <Spinner frame/>
-                :
-                  <PDFViewer width="100%" height='1080'>
-                    <RequestPDF className="pdf"
-                      request={data?.request} 
-                      equipment={data?.equipment}
-                    />
-                  </PDFViewer>
-                }
-              </Route>
+            <Route path="/salesmen-list">
+              <Header />
+              <SalesmenList />
+            </Route>
 
-              <Route path="/salesmen-list">
-                <Header />
-                <SalesmenList />
-              </Route>
+            <Route path="/transport-manager">
+              <Header />
+              <TransportManager />
+            </Route>
 
-              <Route path="/loaner-manager">
-                <Header />
-                <LoanerManager />
-              </Route>
+            <Route path="/loaner-manager">
+              <Header />
+              <LoanerManager />
+            </Route>
 
-              <Route path="/add-loaner">
-                <Header />
-                <AddLoanerView />
-              </Route>
+            <Route path="/add-loaner">
+              <Header />
+              <AddLoanerView />
+            </Route>
 
-              <Route path="/completed">
-                <Header />
-                <Completed />
-              </Route>
+            <Route path="/completed">
+              <Header />
+              <Completed />
+            </Route>
 
-              <Route path="/add-request">
-                <Header />
-                <AddRequestView />
-              </Route>
+            <Route path="/add-request">
+              <Header />
+              <AddRequestView />
+            </Route>
 
-              <Route path="/signIn">
+            <Route path="/signIn">
+              <SignIn />
+            </Route>
+
+            <Route path="/signUp">
+              <SignUp />
+            </Route>
+
+            <Route path="/">
+              {user && <Header />}
+              {!loading && !user ? (
                 <SignIn />
-              </Route>
-
-              <Route path="/signUp">
-                <SignUp />
-              </Route>
-
-              <Route path="/">
-                {user && <Header />}
-                { !loading && !user ? <SignIn /> : !loading && user ? <Home /> : <Spinner frame/> }
-              </Route>
-            </Switch>
+              ) : !loading && user ? (
+                <Home />
+              ) : (
+                <Spinner frame />
+              )}
+            </Route>
+          </Switch>
         </Router>
       </div>
     </ThemeProvider>
   );
-};
+}
 
 export default App;
---
+++ src/Views/table-views/TransportManager.js
@@ -0,0 +1,135 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { useStateValue } from "../../state-management/StateProvider";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from "../../services/firebase";
+import Button from "@mui/material/Button";
+import HomeSkeleton from "../../components/HomeSkeleton";
+import "../../styles/Table.css";
+import { Link } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
+import Dialog from "@mui/material/Dialog";
+import AddRequestView from "../AddRequestView";
+import { RequestsTableHeaderView } from "../../components/TableHeaderViews";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import RequestRow from "./ActiveRequestRow";
+import { Typography } from "@material-ui/core";
+
+// Whole table view:
+export default function TransportManager() {
+  const [{ userProfile }] = useStateValue();
+  const [requests, setRequests] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [openAddTransportView, setOpenAddTransportView] = useState(false);
+
+  const handleCloseAddTansportView = () => {
+    setOpenAddTransportView(false);
+  };
+
+  const handleToggleAddTansportView = () => {
+    setOpenAddTransportView(!openAddTransportView);
+  };
+
+  // Fetch requests from firestore:
+  const fetch = useCallback(async () => {
+    if (userProfile) {
+      const transportQuery = query(
+        collection(db, "branches", userProfile?.branch, "transports"),
+        where("status", "!=", "Completed")
+      );
+
+      onSnapshot(transportQuery, (querySnapshot) => {
+        setRequests(
+          querySnapshot.docs.map((doc) => ({
+            id: doc.data().id,
+            salesman: doc.data().salesman,
+            timestamp: doc.data().timestamp,
+            workOrder: doc.data().workOrder,
+            status: doc.data().status,
+            statusTimestamp: doc.data().statusTimestamp,
+            changeLog: doc.data().changeLog,
+          }))
+        );
+        setTimeout(function () {
+          setLoading(false);
+        }, 1000);
+      });
+    } else {
+      console.log("userProfile not loaded");
+    }
+  }, [userProfile]);
+
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
+
+  // Table UI:
+  return (
+    <React.Fragment>
+      {loading ? (
+        <HomeSkeleton />
+      ) : (
+        <React.Fragment>
+          <div className="tableHead">
+            <Typography
+              variant="h4"
+              color="primary"
+              style={{ marginLeft: 25, marginBottom: 10 }}
+            >
+              Active Setup Requests
+            </Typography>
+
+            <Button
+              color="success"
+              size="small"
+              variant="outlined"
+              startIcon={<AddIcon />}
+              onClick={handleToggleAddTansportView}
+              sx={{ mx: 4, mb: 1, mt: 1 }}
+            >
+              Submit Delivery/Pickup
+            </Button>
+          </div>
+
+          <Dialog
+            onClose={handleCloseAddTansportView}
+            open={openAddTransportView}
+          >
+            <div className="closeButtonContainer">
+              <Button onClick={handleCloseAddTansportView} color="success">
+                <CancelOutlinedIcon />
+              </Button>
+            </div>
+            <div className="addRequestView">
+              <AddRequestView />
+            </div>
+          </Dialog>
+
+          <TableContainer component={Paper} style={{ borderRadius: 10 }}>
+            <Table
+              size="small"
+              aria-label="collapsible table"
+              style={{ margin: 15 }}
+              sx={{ paddingTop: 2 }}
+            >
+              <RequestsTableHeaderView />
+              <TableBody>
+                {requests.map((request) => (
+                  <RequestRow request={request} />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="completed-link">
+            <Link to={"/completed"}>
+              <h3>View completed requests</h3>
+            </Link>
+          </div>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+}
