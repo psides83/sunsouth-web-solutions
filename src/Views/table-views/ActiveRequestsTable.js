6a5c4be4d7d9e9ae632ac7e7fd46a191c28752b6
Payton <psides83@hotmail.com>
2022-05-26 11:32:26 -0500

    ActiveRequestsTable migrated

--- src/Views/table-views/ActiveRequestsTable.js
+++ src/Views/table-views/ActiveRequestsTable.js
@@ -1,22 +1,23 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useStateValue } from "../../state-management/StateProvider";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableContainer from "@material-ui/core/TableContainer";
-import Paper from "@material-ui/core/Paper";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
 import { db } from "../../services/firebase";
-import Button from "@mui/material/Button";
 import HomeSkeleton from "../../components/HomeSkeleton";
 import "../../styles/Table.css";
 import { Link } from "react-router-dom";
-import AddIcon from "@mui/icons-material/Add";
-import Dialog from "@mui/material/Dialog";
 import AddRequestView from "../AddRequestView";
 import { RequestsTableHeaderView } from "../../components/TableHeaderViews";
-import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 import RequestRow from "./ActiveRequestRow";
-import { Typography } from "@material-ui/core";
+import {
+  Button,
+  Dialog,
+  Paper,
+  Table,
+  TableBody,
+  TableContainer,
+  Typography,
+} from "@mui/material";
+import { AddRounded, CancelOutlined } from "@mui/icons-material";
 
 // Whole table view:
 export default function ActiveRequestsTable() {
@@ -86,7 +87,7 @@ export default function ActiveRequestsTable() {
               color="success"
               size="small"
               variant="outlined"
-              startIcon={<AddIcon />}
+              startIcon={<AddRounded />}
               onClick={handleToggleAddRequestView}
               sx={{ mx: 4, mb: 1, mt: 1 }}
             >
@@ -97,7 +98,7 @@ export default function ActiveRequestsTable() {
           <Dialog onClose={handleCloseAddRequestView} open={openAddRequestView}>
             <div className="closeButtonContainer">
               <Button onClick={handleCloseAddRequestView} color="success">
-                <CancelOutlinedIcon />
+                <CancelOutlined />
               </Button>
             </div>
             <div className="addRequestView">
